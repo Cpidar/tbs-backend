4cf3f0d6c4304194b62fbdf5bbb4106a6118b1f8
Cpidar <sayeyesepidar@gmail.com>
2024-12-13 10:16:49 +0330

    product-update batch job for haseb melli

--- src/api/admin/inventory/[sku]/route.ts
+++ src/api/admin/inventory/[sku]/route.ts
@@ -1,6 +1,7 @@
 import type {
     MedusaRequest,
     MedusaResponse,
+    PricingService,
     ProductVariant,
     ProductVariantService,
 } from "@medusajs/medusa"
@@ -12,13 +13,34 @@ export const PUT = async (
 ) => {
     const sku = req.params.sku
     const inventory_quantity = (req.body as UpdateProductVariantInput).inventory_quantity
+    const amount = +((req.body as any).amount)
     const productVariantService: ProductVariantService = req.scope.resolve('productVariantService')
+    const pricingService: PricingService = req.scope.resolve('pricingService')
+    console.log(amount)
+    
+    const variant = await productVariantService.retrieveBySKU(sku, {
+        // region_id: "reg_01HV8YJHZYC979X27ABJDP0Q67",
+        relations: ["prices"]
+    })
 
-    const variant = await productVariantService.retrieveBySKU(sku)
     const variantId = variant.id
+    const prices = variant.prices
+    console.log(prices)
 
+    await productVariantService.updateVariantPrices([{
+        variantId,
+        prices: [{
+            amount,
+            currency_code: 'irr'
+        }]
+    }])
     const updatedVarinat = await productVariantService.update(variantId, { inventory_quantity })
 
-    res.json({ updatedVarinat })
+    const price = await pricingService.getProductVariantPricing(variant, {
+        // region_id: "reg_01HV8YJHZYC979X27ABJDP0Q67"
+    })
+
+
+    res.json({ price })
 
 }
---
+++ src/strategies/variants-update.ts
@@ -0,0 +1,144 @@
+import {
+    AbstractBatchJobStrategy,
+    BatchJobService,
+    CreateBatchJobInput,
+    BatchJob,
+    StoreService,
+    MedusaRequest,
+    ProductVariantService
+} from "@medusajs/medusa"
+import { EntityManager } from "typeorm"
+
+interface ContextData {
+    sku: string
+    quantity: number
+    price: number
+}
+
+class WoocommerceImportStrategy extends AbstractBatchJobStrategy {
+    static batchType = "variatn-update"
+    static identifier = "variatn-update-strategy"
+
+    protected batchJobService_: BatchJobService
+    protected productVariantService_: ProductVariantService
+    protected manager_: EntityManager
+    protected transactionManager_: EntityManager
+    protected store_: StoreService
+
+    constructor(
+        {
+            manager,
+            storeService,
+            productVariantService,
+            batchJobService
+        },
+        options
+    ) {
+        super({
+            manager,
+            storeService,
+            productVariantService,
+            batchJobService
+        })
+
+
+        this.manager_ = manager
+        this.store_ = storeService
+        this.batchJobService_ = batchJobService
+        this.productVariantService_ = productVariantService
+    }
+
+
+    async prepareBatchJobForProcessing(
+        batchJob: CreateBatchJobInput,
+        req: MedusaRequest
+    ): Promise<CreateBatchJobInput> {
+
+        console.log(req.body)
+
+        return batchJob
+    }
+
+    async preProcessBatchJob(batchJobId: string): Promise<void> {
+        return
+    }
+
+
+    async processJob(batchJobId: string): Promise<void> {
+        return this.atomicPhase_(async (manager) => {
+
+            const batchJob = (await this.batchJobService_
+                .withTransaction(manager)
+                .retrieve(batchJobId))
+
+            const productVariantServiceTx = this.productVariantService_
+                .withTransaction(manager)
+
+            const context = batchJob.context as { data: ContextData[] }
+            const data = context.data
+            const count = data.length
+
+            data.forEach(async d => {
+                const { sku, quantity, price } = d
+                
+                const variant = await productVariantServiceTx.retrieveBySKU(sku, {
+                    // region_id: "reg_01HV8YJHZYC979X27ABJDP0Q67",
+                    relations: ["prices"]
+                })
+                const variantId = variant.id
+
+                await productVariantServiceTx.updateVariantPrices([{
+                    variantId,
+                    prices: [{
+                        amount: price,
+                        currency_code: 'irr'
+                    }]
+                }])
+
+                await productVariantServiceTx.update(variantId, { inventory_quantity: quantity })
+            })
+
+
+
+            await this.batchJobService_
+                .withTransaction(manager)
+                .update(batchJobId, {
+                    result: {
+                        advancement_count: count,
+                        count,
+                        stat_descriptors: [
+                            {
+                                key: "variatn-update-count",
+                                name: "Number of product variants to update",
+                                message:
+                                    `${count} product(s) will be updated.`,
+                            },
+                        ],
+                    },
+                })
+
+
+
+
+        })
+    }
+
+    buildTemplate(): Promise<string> {
+        throw new Error("Method not implemented.")
+    }
+    protected async shouldRetryOnProcessingError(
+        batchJob: BatchJob,
+        err: unknown
+    ): Promise<boolean> {
+        return true
+    }
+    protected async handleProcessingError<T>(
+        batchJobId: string,
+        err: unknown,
+        result: T
+    ): Promise<void> {
+        // different implementation...
+    }
+}
+
+export default WoocommerceImportStrategy
